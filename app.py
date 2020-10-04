1b47edd8c7901f1b2b149081d4f0c2c87b3c96f8
jake202020 <slepjake@gmail.com>
2020-10-04 17:38:55 -0400

    environ vars

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'postgresql:///space_db')
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", SECRET_KEY)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "thisisthelocalsecretkey")
 
 connect_db(app)
 
