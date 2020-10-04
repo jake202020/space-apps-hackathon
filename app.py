6105e66d5621d09bcd28e8cd09843243b80d6011
jake202020 <slepjake@gmail.com>
2020-10-04 18:10:35 -0400

    CORS

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models import db, connect_db, Resource
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'postgresql:///space_db')
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "thisisthelocalsecretkey")
